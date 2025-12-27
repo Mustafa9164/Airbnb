d1c70951fbc3b25bb33bf16536f525ba380fca3a
Mustafa9164 <mohdmustafa9164@gmail.com>
2025-12-27 19:43:39 +0530

    first commit

---
+++ src/main/java/com/airbnb/User.java
@@ -0,0 +1,25 @@
+package com.airbnb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
